52b91cdebc32f0b7bdd000d9d8ded2879bca5bd4
Siavash Golchoobian <siavash.golchoobian@gmail.com>
2024-11-02 19:51:07 +0330

    correct catalog path

--- SqlDeepSynchronizer.ps1
+++ SqlDeepSynchronizer.ps1
@@ -6,42 +6,74 @@ Deploy
     SqlDeepDB
     PowershellTools
 #>
-[string]$LocalRepositoryRoot='E:\Log\SqlDeep'
-[string]$myCurrentPath='C:\Users\Siavash\Dropbox\SQL Deep\Tools\SqlDeep Projects\SqlDeep-Synchronizer'
+[string]$LocalRepositoryPath='E:\Log\SqlDeep'
 
 enum SqlDeepRepositoryItemCategory {
     SqlDeepCatalog
     SqlDeepDatabase
     SqlDeepPowershellTools
+    SqlDeepTsqlScript
 }
 Class WebRepositoryItem {
     [SqlDeepRepositoryItemCategory]$Category
-    [string]$FileName
     [string]$FileURI
-    [string]$LocalFilePath
+    [string]$LocalFileName
+    [string]$LocalFolderPath
    
-    WebRepositories([SqlDeepRepositoryItemCategory]$Category,[string]$FileName,[string]$FileURI){
+    WebRepositoryItem([SqlDeepRepositoryItemCategory]$Category,[string]$FileURI,[string]$LocalFolderPath,[string]$LocalFileName){
         Write-Verbose 'WebRepositoryItem object initializing started'
         $this.Category=$Category
-        $this.FileName=$FileName
         $this.FileURI=$FileURI
-        $this.LocalFilePath=$null
+        $this.LocalFolderPath=$LocalFolderPath
+        $this.LocalFileName=$LocalFileName
         Write-Verbose 'WebRepositoryItem object initialized'
     }
 }
 
-[WebRepositoryItem[]]$myWebRepositoryCollection=$null
-[WebRepositoryItem]$myWebRepositoryItem=$null
-
-$myWebRepositoryItem=[WebRepositoryItem]::New([WebRepositoryItem]::SqlDeepCatalog,'SqlDeepCatalog.txt','',$null)
-#Initialize Repository
-$mySqlDeepRepositories
-
-#Create Local Repository Root folder if not exists
-if((Test-Path -Path $LocalRepositoryRoot) -eq $false) {
-    New-Item -ItemType Directory -Path $LocalRepositoryRoot -Force
+function DownloadFile {
+    [OutputType([bool])]
+    param (
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true,HelpMessage="URI address to download")][string]$URI,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true,HelpMessage="Save downloaded file to this folder path")][string]$FolderPath,
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true,HelpMessage="Save downloaded file as this name")][string]$FileName
+    )
+    begin{
+        #Create SaveToFolderPath if not exists
+        [bool]$myAnswer=$false
+        if((Test-Path -Path $FolderPath) -eq $false) {
+            New-Item -ItemType Directory -Path $FolderPath -Force
+        }
+    }
+    process{
+        try {
+            if((Test-Path -Path $FolderPath) -eq $true) {
+                Invoke-WebRequest -Uri $URI -OutFile ($FolderPath+'\'+$FileName)
+                $myAnswer=(Test-Path -Path ($FolderPath+'\'+$FileName))
+            }else{
+                $myAnswer=$false
+            }
+        }catch{
+            $myAnswer=$false
+        }
+        return $mtAnswer
+    }
+    end{}
 }
-
-#='https://github.com/SiavashGolchoobian/SqlDeep/raw/refs/heads/main/Assets/Release/Latest/SqlDeep.dacpac'
+#===============Parameters
+[string]$mySqlDeepOfficialCatalogURI=$null;
+[WebRepositoryItem[]]$myWebRepositoryCollection=$null;
+[WebRepositoryItem]$myWebRepositoryItem=$null;
+#===============Constants
+$mySqlDeepOfficialCatalogURI='https://raw.githubusercontent.com/SiavashGolchoobian/SqlDeep-Synchronizer/refs/heads/main/SqlDeepCatalog.json'
+$myWebRepositoryItem=[WebRepositoryItem]::New([SqlDeepRepositoryItemCategory]::SqlDeepCatalog,$mySqlDeepOfficialCatalogURI,$LocalRepositoryPath,'SqlDeepCatalog.json')
+$myWebRepositoryCollection+=($myWebRepositoryItem)
+#Download Catalog file(s)
+$myWebRepositoryCollection | Where-Object -Property Category -eq SqlDeepCatalog | ForEach-Object{DownloadFile -URI ($_.FileURI) -FolderPath ($_.LocalFolderPath) -FileName ($_.LocalFileName)}
+#Fill RepositoryCollection via Catalog file(s)
+foreach ($myWebRepositoryItem in ($myWebRepositoryCollection | Where-Object -Property Category -eq SqlDeepCatalog)) {
+    $myResult=Get-Content -Raw -Path ($myWebRepositoryItem.LocalFolderPath+'\'+$myWebRepositoryItem.LocalFileName) | ConvertFrom-Json
+    $myResult.library.SqlDeepPowershellTools    | Where-Object -Property uri -ne $null | ForEach-Object{$myWebRepositoryCollection+=([WebRepositoryItem]::New([SqlDeepRepositoryItemCategory]::SqlDeepPowershellTools,($_.uri),$LocalRepositoryPath,($_.name)))}
+    $myResult.library.SqlDeepDatabase           | Where-Object -Property uri -ne $null | ForEach-Object{$myWebRepositoryCollection+=([WebRepositoryItem]::New([SqlDeepRepositoryItemCategory]::SqlDeepDatabase,($_.uri),$LocalRepositoryPath,($_.name)))}
+    $myResult.library.SqlDeepTsqlScript         | Where-Object -Property uri -ne $null | ForEach-Object{$myWebRepositoryCollection+=([WebRepositoryItem]::New([SqlDeepRepositoryItemCategory]::SqlDeepTsqlScript,($_.uri),$LocalRepositoryPath,($_.name)))}
+}
 #Download Repository Contents
-Invoke-WebRequest -Uri $myLocalRepositoryRoot -OutFile .\helloworld.ps1
