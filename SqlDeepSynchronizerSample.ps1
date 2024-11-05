eccab8ae37167bea5c671e2d90a67b278fb60898
Siavash Golchoobian <siavash.golchoobian@gmail.com>
2024-11-05 15:23:01 +0330

    Sign file

--- SqlDeepSynchronizerSample.ps1
+++ SqlDeepSynchronizerSample.ps1
@@ -32,3 +32,4 @@ $myRepositoryItems=ConvertFrom-RepositoryItemsFile -FilePath 'E:\Log\SqlDeep\Sql
 
 #.\SqlDeepSynchronizer.ps1 -DownloadAssets -LocalRepositoryPath 'E:\Log\SqlDeep'
 #.\SqlDeepSynchronizer.ps1 -SyncScriptRepository -LocalRepositoryPath 'E:\Log\SqlDeep' -SqlDeepRepositoryItemsFileName 'SqlDeepCatalog.json.result' -ConnectionString 'Data Source=172.18.3.49,2019;Initial Catalog=SqlDeep;TrustServerCertificate=True;Encrypt=True;User=sa;Password=Armin1355$'
+#.\SqlDeepSynchronizer.ps1 -SyncDatabaseModule -LocalRepositoryPath 'E:\Log\SqlDeep' -SqlDeepRepositoryItemsFileName 'SqlDeepCatalog.json.result' -ConnectionString 'Data Source=172.18.3.49,2019;Initial Catalog=SqlDeep;TrustServerCertificate=True;Encrypt=True;User=sa;Password=Armin1355$'
