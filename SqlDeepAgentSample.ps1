632f81cfa21c36ab49069ab310c3e7cd896adda1
Siavash Golchoobian <siavash.golchoobian@gmail.com>
2024-11-07 01:57:25 +0330

    Update SqlDeepAgentSample.ps1

--- SqlDeepAgentSample.ps1
+++ SqlDeepAgentSample.ps1
@@ -1,3 +1,3 @@
 .\SqlDeepAgent.ps1 -DownloadAssets -LocalRepositoryPath 'E:\Log\SqlDeep'
 .\SqlDeepAgent.ps1 -SyncDatabaseModule -LocalRepositoryPath 'E:\Log\SqlDeep' -SqlDeepRepositoryItemsFileName 'SqlDeepCatalog.json.result' -ConnectionString 'Data Source=172.18.3.49,2022;Initial Catalog=SqlDeep;TrustServerCertificate=True;Encrypt=True;User=sa;Password=P@$$W0rd'
-.\SqlDeepAgent.ps1 -SyncScriptRepository -LocalRepositoryPath 'E:\Log\SqlDeep' -SqlDeepRepositoryItemsFileName 'SqlDeepCatalog.json.result' -ConnectionString 'Data Source=172.18.3.49,2019;Initial Catalog=SqlDeep;TrustServerCertificate=True;Encrypt=True;User=sa;Password=P@$$W0rd'
+.\SqlDeepAgent.ps1 -SyncScriptRepository -LocalRepositoryPath 'E:\Log\SqlDeep' -SqlDeepRepositoryItemsFileName 'SqlDeepCatalog.json.result' -ConnectionString 'Data Source=172.18.3.49,2022;Initial Catalog=SqlDeep;TrustServerCertificate=True;Encrypt=True;User=sa;Password=P@$$W0rd'
